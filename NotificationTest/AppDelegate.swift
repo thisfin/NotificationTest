f804b38638254a1e31499890d4b3ce7d8f750c65
文优 <yi.li@hipac.cn>
2020-12-04 20:12:16 +0800

    update

--- NotificationTest/AppDelegate.swift
+++ NotificationTest/AppDelegate.swift
@@ -18,8 +18,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 JPAuthorizationOptions.providesAppNotificationSettings.rawValue)
         }, delegate: self)
 
+        JPUSHService.registrationIDCompletionHandler { resCode, registrationID in
+            print("\(resCode) \(registrationID ?? "errorID")")
+        }
+
+        JPUSHService.setAlias("wenyou", completion: { iResCode, iAlias, seq in
+            print("\(iResCode) \(iAlias ?? "") \(seq)")
+        }, seq: 0)
+
         JPUSHService.setup(withOption: launchOptions, appKey: "1bc78bc2376f2a3be66f97d5", channel: "Publish channel", apsForProduction: false)
-//"3757a70808011905377359a4"
+        // "3757a70808011905377359a4"
         return true
     }
 
@@ -55,6 +63,7 @@ extension AppDelegate: JPUSHRegisterDelegate {
         ()
     }
 
+    // iOS 12
     func jpushNotificationAuthorization(_ status: JPAuthorizationStatus, withInfo info: [AnyHashable: Any]!) {
         ()
     }
